c1befc2a5f115233a7d099c37d93f2ce206ece03
shraddhavr <hardeshraddha57@gmail.com>
2022-12-18 18:23:04 +0530

    started

--- index.js
+++ index.js
@@ -1,5 +1,16 @@
 import  express from "express";
 const app=express();
+app.use(express.json());
+
+app.get('/name',(req,res)=>{
+    res.send("Shradha")
+})
+app.get('/address',(req,res)=>{
+    res.send("Deolali-Pravara,tal-Rahuri,dis:A-Nagar")
+})
+app.get('/college',(req,res)=>{
+    res.send("Rajarshi Shahu Mahavidyalaya Deolali-Pravara ")
+})
 
 
 app.listen(5000,()=>{
